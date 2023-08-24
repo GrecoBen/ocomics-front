d3cad1f5a0fde7ac9d6a043934af19b3432d30b6
NguyenThanh-Binh <thanh-binh.nguyen@oclock.school>
2023-08-24 17:36:17 +0200

    feat-Characters

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -9,20 +9,22 @@ const contentStyle: React.CSSProperties = {
   background: 'grey',
 };
 
+const Slider = [
+  'https://i.ibb.co/5hxqBVw/ai-generated-8098396-1280.png',
+  'https://i.ibb.co/n3Ly567/ai-generated-8154910-1280.jpg',
+  'https://i.ibb.co/dBQx2gM/captain-america-6789190-1280.jpg',
+  "https://i.ibb.co/XyQn0zj/marvel-3165096-1280.jpg",
+  "https://i.ibb.co/Y0ky8kj/spiderman-8158916-1280.png",
+  "https://i.ibb.co/bJRxSCr/the-incredible-hulk-7471339-1280.jpg"
+];
+
 const App: React.FC = () => (
   <Carousel autoplay>
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
+    {Slider.map((imageUrl, index) => (
+      <div key={index}>
+        <img src={imageUrl} alt={`Slide-${index}`} style={{ ...contentStyle, width: '100%' }} />
+      </div>
+    ))}
   </Carousel>
 );
 

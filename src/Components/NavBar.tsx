78b5d7e5a3e8c2aff788a2e233a3c0c00bbdb8f1
NguyenThanh-Binh <thanh-binh.nguyen@oclock.school>
2023-09-05 20:20:25 +0200

    Feat-New-Logout

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,41 +1,61 @@
 import React from 'react'; 
 import Logo from '../assets/Logo.png'; 
-import { Link } from 'react-router-dom'; 
-import { UserOutlined } from '@ant-design/icons'; 
+import { UserOutlined } from '@ant-design/icons';
+import useAuth from '../hooks/useAuth'; 
+import { useNavigate, Link } from 'react-router-dom';
 
-// Définition des types des propriétés (props) attendues par le composant Navbar
 type NavbarProps = {
-  isAuthenticated: boolean; // Un booléen pour vérifier si l'utilisateur est authentifié
-  onLogout: () => void; // Une fonction de rappel pour gérer la déconnexion
+  isAuthenticated: boolean; 
 }
 
 // Définition du composant Navbar en tant que composant fonctionnel React
-const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, onLogout }) => {
+const Navbar: React.FC<NavbarProps> = ({ isAuthenticated }) => {
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+  console.log('authtototo', auth);
+
+  const onLogout = () => {
+    // Supprime le token d'accès du localStorage
+    localStorage.removeItem('accessToken');
+
+    // Réinitialise l'état de l'authentification
+    const authData = {
+      auth: false,
+      email: '',
+      roles: [],
+      accessToken: '',
+    };
+    setAuth(authData);
+
+    // Redirige l'utilisateur vers la page d'accueil (ou toute autre page souhaitée)
+    navigate('/');
+  };
+
+  isAuthenticated = auth.auth
   return (
-    // La structure HTML de la barre de navigation
-    <nav className="container mx-auto bg-yellow-500"> {/* Un élément de navigation avec une classe CSS */}
-      <div className="flex h-14 items-center justify-between border-slate-200"> {/* Un conteneur flexible avec des éléments centrés et justifiés */}
+    <nav className="container mx-auto bg-yellow-500"> 
+      <div className="flex h-14 items-center justify-between border-slate-200"> 
         <Link to="/"> {/* Un lien de navigation vers la page d'accueil */}
-          <img className="h-12 w-24 rounded-md cursor-pointer" src={Logo} alt="" /> {/* Une image de logo */}
+          <img className="h-12 w-24 rounded-md cursor-pointer" src={Logo} alt="" /> 
         </Link>
-        <ul className="hidden gap-8 text-sm font-semibold md:flex"> {/* Une liste non ordonnée pour les liens de navigation */}
+        <ul className="hidden gap-8 text-sm font-semibold md:flex">
           <li>
-            <Link to="/Comics" className='hover:bg-yellow-600'>Comics</Link> {/* Lien vers la page "Comics" */}
+            <Link to="/Comics" className='hover:bg-yellow-600'>Comics</Link>
           </li>
           <li>
 
-            <Link to="/Personnages" className='hover:bg-yellow-600'>Personnages</Link> {/* Lien vers la page "Personnages" */}
+            <Link to="/Personnages" className='hover:bg-yellow-600'>Personnages</Link> 
           </li>
           {isAuthenticated ? ( // Condition pour vérifier si l'utilisateur est authentifié
             <>
               <li>
-                <Link to="/possede" className='hover:bg-yellow-600'>Je possède</Link> {/* Lien vers la page "Je possède" */}
+                <Link to="/possede" className='hover:bg-yellow-600'>Je possède</Link> 
               </li>
               <li>
-                <Link to="/recherche" className='hover:bg-yellow-600'>Je recherche</Link> {/* Lien vers la page "Je recherche" */}
+                <Link to="/recherche" className='hover:bg-yellow-600'>Je recherche</Link>
               </li>
               <li>
-                <button onClick={onLogout} className='hover:bg-yellow-600'>Se déconnecter</button> {/* Bouton pour se déconnecter */}
+                <button onClick={onLogout} className='hover:bg-yellow-600'>Se déconnecter</button>
               </li>
               <li>
                 {isAuthenticated && ( // Afficher l'icône d'utilisateur uniquement si l'utilisateur est authentifié
@@ -46,17 +66,17 @@ const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, onLogout }) => {
           ) : ( // Si l'utilisateur n'est pas authentifié, afficher les liens d'inscription et de connexion
             <>
               <li>
-                <Link to="/register" className='hover:bg-yellow-600'>Inscription</Link> {/* Lien vers la page d'inscription */}
+                <Link to="/register" className='hover:bg-yellow-600'>Inscription</Link>
               </li>
               <li>
-                <Link to="/login" className='hover:bg-yellow-600'>Se connecter</Link> {/* Lien vers la page de connexion */}
+                <Link to="/login" className='hover:bg-yellow-600'>Se connecter</Link>
               </li>
             </>
           )}
         </ul>
-        <div className="flex gap-4"> {/* Un conteneur flexible pour les éléments à droite de la barre de navigation */}
+        <div className="flex gap-4"> 
           <input placeholder="recherche..." className="rounded-md w-24 border-2 border-slate-200 md:w-32" type="text" /> {/* Champ de recherche */}
-          <button className="block md:hidden"> {/* Bouton pour afficher le menu sur les écrans mobiles */}
+          <button className="block md:hidden"> 
             <svg className="h-6 w-6 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path></svg>
           </button>
         </div>

7eb4b567e6058eb64c531da48d5ae25a6f978198
GrecoBen <greco.benjamin@orange.fr>
2023-09-10 13:25:37 +0200

    Feat-HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AuthProvider>
       <Routes>
       <Route path="/*" element={<App />} />
       </Routes>
     </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
